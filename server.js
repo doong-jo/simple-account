db6be82d3bfc325587a47433de1a0259356679fe
Sungdong Jo <sdong001@gmail.com>
2019-08-28 23:20:42 +0900

    beautify & all get routing going to response index

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const
-    express = require('express');
+    express = require('express'),
     app = express(),
     reload = require('reload'),
     watch = require('watch'),
@@ -7,18 +7,20 @@ const
     path = require('path'),
 
     staticServe = express.static(__dirname + '/src');
-    port = 8090;
+port = 8090;
 
+//---------------------------------------------------------------------
 if (process.env.NODE_ENV == 'production') {
     console.log("Production Mode");
 } else if (process.env.NODE_ENV == 'development') {
     reload(app).then(function (reloadReturned) {
-        watch.watchTree(path.join(__dirname, 'src'), { 'interval': 0.5 }, function (f, curr, prev) {
-        // Fire server-side reload event
-        reloadReturned.reload();
+        watch.watchTree(path.join(__dirname, 'src'), {
+            'interval': 0.5
+        }, (f, curr, prev) => {
+            reloadReturned.reload();
         })
     }).catch(function (err) {
-        console.error('Reload could not start, could not start server/sample app', err);
+        console.error('Reload could not start', err);
     });
 
     console.log("Development Mode");
@@ -26,8 +28,8 @@ if (process.env.NODE_ENV == 'production') {
 
 
 app.use('/', staticServe);
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'index');
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/src/index.html');
 })
 
 app.listen(port, () => {
