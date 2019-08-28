914e9f3d84ebecbbe7077dd9ae3411fb54fb8b26
Sungdong Jo <sdong001@gmail.com>
2019-08-28 23:10:03 +0900

    modify js->mjs (module scripts) and set node ENV

--- server.js
+++ server.js
@@ -6,20 +6,29 @@ const
 
     path = require('path'),
 
-    staticServe = express.static(path.join(__dirname, 'src'));
+    staticServe = express.static(__dirname + '/src');
     port = 8090;
 
-reload(app).then(function (reloadReturned) {
-    watch.watchTree(path.join(__dirname, 'src'), { 'interval': 0.5 }, function (f, curr, prev) {
+if (process.env.NODE_ENV == 'production') {
+    console.log("Production Mode");
+} else if (process.env.NODE_ENV == 'development') {
+    reload(app).then(function (reloadReturned) {
+        watch.watchTree(path.join(__dirname, 'src'), { 'interval': 0.5 }, function (f, curr, prev) {
         // Fire server-side reload event
         reloadReturned.reload();
+        })
+    }).catch(function (err) {
+        console.error('Reload could not start, could not start server/sample app', err);
     });
-}).catch(function (err) {
-    console.error('Reload could not start, could not start server/sample app', err);
-});
+
+    console.log("Development Mode");
+}
+
 
 app.use('/', staticServe);
-app.use('*', staticServe);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'index');
+})
 
 app.listen(port, () => {
     console.log(`Listening to ${port}...`);
--- src/app.mjs
+++ src/app.mjs
@@ -1,8 +1,8 @@
-import Util from './services/util.js';
+import Util from './services/util.mjs';
 
-import Main from './views/pages/Main.js';
-import Signup from './views/pages/Signup.js';
-import Login from './views/pages/Login.js';
+import Main from './views/pages/Main.mjs';
+import Signup from './views/pages/Signup.mjs';
+import Login from './views/pages/Login.mjs';
 
 // 참조 : https://dev.to/rishavs/making-a-single-page-app-in-ye-good-olde-js-es6-3eng
 const routes = {
