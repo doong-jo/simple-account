876f49fc722a883abfa4c0f89fea662fefbadb51
Sungdong Jo <sdong001@gmail.com>
2019-08-29 23:18:09 +0900

    add making element node functions

--- src/services/util.mjs
+++ src/services/util.mjs
@@ -11,7 +11,80 @@ const Util = {
         link.rel = "stylesheet";
 
         document.getElementsByTagName("head")[0].appendChild( link );
-    }
+    },
+
+    getNewElement: function (type, args) {
+        return this.makeElement[type](args);
+    },
+
+    makeInput: (args) => {
+        const 
+            i = document.createElement("input"),
+            { inputType, nameAndId, placeholder, value, disabled } = args;
+    
+        i.type = inputType;
+        i.name = nameAndId;
+        i.id = nameAndId;
+
+        if (inputType !== 'checkbox') {
+            i.placeholder = placeholder;
+        } else {
+            i.value = value;
+            i.disabled = disabled;
+        }
+    
+        return i;
+    },
+
+    makeLabel: (args) => {
+        const l = document.createElement("label");
+
+        l.htmlFor = args.for;
+        l.innerHTML = args.innerHTML;
+
+        return l;
+    },
+
+    makeSelectAndOption: (args) => {
+        const 
+            s = document.createElement("select"),
+            { selectedInd, values } = args;
+
+
+        values.forEach((v, i) => {
+            const o = document.createElement("option");
+            o.value = v;
+            o.innerHTML = v;
+
+            if( i === selectedInd ) { o.selected = true; }
+
+            s.appendChild(o);
+        });
+
+        return s;
+    },
+
+    makeButton: (args) => {
+        const
+            btn = document.createElement('button'),
+            { text, doAction, className } = args;
+
+        btn.innerHTML = text;
+        btn.onclick = doAction;
+        btn.className = className || "";
+
+        return btn;
+    },
+
+    makeElementByType: function() {
+        return {
+            'input' : this.makeInput,
+            'label' : this.makeLabel,
+            'select' : this.makeSelectAndOption,
+            'checkbox' : this.makeInput,
+            'button' : this.makeButton,
+        }
+    },
 };
 
 export default Util;
