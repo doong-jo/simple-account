171fe9349904d5b4ac2032a9e60c7510810238a9
Sungdong Jo <sdong001@gmail.com>
2019-08-30 20:52:43 +0900

    Styling TagList input, Add TagList remove logic

--- src/views/components/taglist.mjs
+++ src/views/components/taglist.mjs
@@ -4,47 +4,72 @@ class TagList {
         this.tagElement.className = "tag-container vertical-margin";
         this.tagElement.id = id;
 
-        this.removeSVGPath = "public/svg/close.svg";
-        this.KEY_COMMA = "Comma";
+        this.CLOSE_SVG_PATH = "public/svg/close.svg";
 
-        this.values = [];
+        this.tagData = defaultValues;
 
-        this.init(defaultValues);
+        this.init();
     }
 
-    init(defaultValues) {
-        this.values = defaultValues;
+    init() {
         this.tagInput = document.createElement('input');
-
         this.tagInput.type = "text";
 
-        for(var i=0; i<defaultValues.length; i++) {
-            this.makeTag(defaultValues[i]);
+        for(var i=0; i<this.tagData.length; i++) {
+            this.makeTag(this.tagData[i]);
         }
-        
         this.tagElement.appendChild(this.tagInput);
 
         this.registerEvents();
     }
 
     registerEvents() {
-        let tagStr = '';
-        
-        this.tagInput.addEventListener('keyup', e => {
-            const inp = e.key;
-            // only characters and comma and backspace
+        this.tagInput.addEventListener('input', this.inputEvent.bind(this));
+        this.tagInput.addEventListener('keydown', this.keyDownEvent.bind(this));
+    }
 
-            if( ( 
-                    /[a-zA-Z0-9-_,ㄱ-ㅎㅏ-ㅣ]/.test(inp) || 
-                    e.key.length == 1
-                ) && e.key === "," ) {
-                if( this.tagInput.value !== ',' ) {
-                    this.makeTag(this.tagInput.value.slice(0, -1));   
-                }
-
-                this.tagInput.value = '';
+    inputEvent(e) {
+        const inp = e.data;
+        // only characters and comma and backspace
+        console.log(e.data);
+        if( ( /[a-zA-Z0-9-_,ㄱ-ㅎㅏ-ㅣ]/.test(inp) || 
+                inp.length === 1
+            ) && inp === "," ) {
+            if( this.tagInput.value !== ',' ) {
+                this.addTag();
             }
-        });
+
+            this.tagInput.value = '';
+        } else if( inp === "Backspace" &&
+            this.tagData.length > 0 &&
+            this.tagInput.value === '' ) {
+            this.removeLastTag();
+        }
+    }
+
+    keyDownEvent(e) {
+        const inp = e.key;
+
+        if( inp === "Backspace" &&
+            this.tagData.length > 0 &&
+            this.tagInput.value === '' ) {
+            this.removeLastTag();
+
+            e.preventDefault();
+        }
+    }
+
+    removeLastTag() {
+        this.tagInput.value = this.tagData.pop();
+
+        const tagBoxes = document.getElementsByClassName("tag-box");
+        tagBoxes[tagBoxes.length - 1].remove();
+    }
+
+    addTag() {
+        const tagStr = this.tagInput.value.slice(0, -1);
+        this.makeTag(tagStr);   
+        this.tagData.push(tagStr);
     }
 
     makeTag(tagStr) {
@@ -54,17 +79,17 @@ class TagList {
             img = document.createElement('img');
         
         span.innerHTML = tagStr;
-        img.src = this.removeSVGPath;
+        img.src = this.CLOSE_SVG_PATH;
         img.onclick = (e) => {
             e.target.parentElement.remove();
         };
-
         div.className = "inline tag-box";
+
         div.appendChild(span);
         div.appendChild(img);
 
         const tagBoxes = document.getElementsByClassName("tag-box");
-        console.log('tagBoxes', tagBoxes);
+
         if( tagBoxes.length < 1 ) {
             this.tagElement.appendChild(div);
         } else {
@@ -73,16 +98,8 @@ class TagList {
         }
     }
 
-    add() {
-        // this.tagElement.appendChild();
-    }
-
-    delete() {
-        // this.tagElement.removeChild();
-    }
-
     get tags() {
-        return this.values;
+        return this.tagData;
     }
 
     get element() {
