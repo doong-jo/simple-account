3c1901c2eb06dde2c7cdf3090115b217b9566566
Sungdong Jo <sdong001@gmail.com>
2019-08-30 21:57:53 +0900

    Make modal assemble logic

--- src/views/components/modal.mjs
+++ src/views/components/modal.mjs
@@ -1,17 +1,15 @@
+import NodeBuilder from '../../services/NodeBuilder.mjs';
+
 class Modal {
     constructor(id, options = {}) {
         this.options = options;
 
+        this.elementId = id;
         this.init(id);
     }
 
     init(id) {
         this.modalContainer = document.getElementById(id);
-
-        const
-            title = document.createElement('div'),
-            content = document.createElement('div');
-
         this.makeModal(this.options);
     }
 
@@ -20,21 +18,40 @@ class Modal {
 
         this.modalView = /*html*/`
             <div class="modal-title">
-                <span class="modal-title-text">${options.title}</span><img src="public/svg/close.svg" />
+                <span class="modal-title-text">${options.title}</span>
+                <img src="public/svg/close.svg" />
             </div>
             <div class="modal-content">
                 ${options.content}
             </div>
-            <div class="modal-footer">
-
-            </div>
+            <div class="modal-footer"></div>
         `;
+
+        this.modalContainer.innerHTML = this.modalView;
+
+        this.setContent(options);
+        this.setButtons(options);
 
         return this.modalView;
     }
 
-    setContent(htmlStr) {
+    setContent(options) {
+        const content = document.querySelector(`#${elementId} .modal-content`);
 
+        content.innerHTML = options.content;
+    }
+
+    setButtons(options) {
+        const footer = document.querySelector(`#${elementId} .modal-footer`);
+
+        if( options.confirmBtn ) {
+            const btn = NodeBuilder.makeButton(options.confirmBtn);
+            footer.appendChild(btn);
+        }
+        if( options.cancleBtn ) {
+            const btn = NodeBuilder.makeButton(options.cancleBtn);
+            footer.appendChild(btn);
+        }
     }
 
     toggle() {
