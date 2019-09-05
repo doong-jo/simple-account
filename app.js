0c63d461d6b6111f9f21ff4e2d12c9206156b174
doong-jo <sdong001@gmail.com>
2019-09-05 16:36:54 +0900

    feat: 회원가입, 로그인, 로그아웃 라우팅
    각각 POST 메소드로 라우팅하였음
    API를 작성하고 이를 연결해야하는 작업이 필요함
    
    close #29

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const logger = require('morgan');
 
 const app = express();
@@ -21,11 +22,14 @@ app.use(express.urlencoded({
     extended: false,
 }));
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(staticServe);
-app.use(notFoundHandler);
-app.use(serverInternalHandler);
 
 app.use('/', mainRouter);
+
+app.use(notFoundHandler);
+app.use(serverInternalHandler);
 
 app.listen(port, () => {
     console.log(`Listening ${port}...`);
--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,30 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('login', (req, res) => {
+// const userAPI = 
 
+router.post('/login', (req, res) => {
+    // TODO: 아이디와 비밀번호 일치 확인
+    console.log('route login');
+    const { id, pwd } = req.body;
+    console.log('login info', { id, pwd });
+
+    res.send({ result: true });
 });
 
-router.post('signup', (req, res) => {
+router.post('/logout', (req, res) => {
+    // TODO: 회원가입 API 연결
+    console.log('route logout');
 
+    res.send({ result: true });
+});
+
+router.post('/signup', (req, res) => {
+    // TODO: 회원가입 API 연결
+    console.log('route signup');
+    console.log('data', req.body);
+
+    res.send({ result: true });
 });
 
 module.exports = router;
